367cb1cbba7835f9b473c681098eccfed0090d92
ZhangTianxiang <ztx97@qq.com>
2019-09-15 21:30:37 +0800

    add singleton

--- util.py
+++ util.py
@@ -3,6 +3,14 @@ from os import path
 import config
 
 
+class Singleton(object):  # 单例
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            orig = super(Singleton, cls)
+            cls._instance = orig.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
 def plugin_dir(file):
     parent, now = path.split(file)
     if now == '__init__.py':
